1b15f7526199ce55b5361245ec35245522655378
jackmcc08 <jackmcc08@gmail.com>
2021-03-17 15:00:59 +0000

    completed challenge POST-GET pattern.
    Co-authored-by: TataSher <77491121+TataSher@users.noreply.github.com>

--- app.rb
+++ app.rb
@@ -1,9 +1,21 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  enable :sessions
+
   get '/' do
-    'Testing infrastructure working!'
+    erb(:index)
   end
 
-end
+  get '/play' do
+    @name_1 = session[:player_1]
+    @name_2 = session[:player_2]
+    erb :play
+  end
 
+  post '/names' do
+    session[:player_1] = params[:Player_1]
+    session[:player_2] = params[:Player_2]
+    redirect '/play'
+  end
+end
--- spec/features/names_spec.rb
+++ spec/features/names_spec.rb
@@ -1,7 +1,10 @@
 feature 'Enter names' do
   scenario 'User opens a page' do
     visit('/')
-    fill_in 'Player 1', with: 'Bill'
-    expect(page).to have_text 'Bill'
+    fill_in 'Player_1', with: 'Jack'
+    fill_in 'Player_2', with: 'Nata'
+    click_button('Submit')
+    expect(page).to have_text 'Jack'
+    expect(page).to have_text 'Nata'
   end
 end
